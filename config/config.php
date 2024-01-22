ac8f67cf91b79716b1909cc359937481a41c4b24
Dasha Syvyuk <35842053+DashaSyvyuk@users.noreply.github.com>
2024-01-22 18:11:14 +0200

    [GG-392] add grace period (#7)

--- config/config.php
+++ config/config.php
@@ -4,6 +4,7 @@ return [
     'expire' => [
         'unit' => env('SAVING_QUOTE_EXPIRE_UNIT', 'day'),
         'value' => env('SAVING_QUOTE_EXPIRE_VALUE', 7),
+        'grace_period' => env('SAVING_QUOTE_EXPIRE_GRACE_PERIOD', 0),
     ],
     'mailable' => null, // Class name of the mailable to send after a quote is saved.
     'validator' => null,
