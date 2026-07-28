e44def2606d82d8553820ce39c17e1eea83d58cc
Alex Andreiev <darkwasabj@gmail.com>
2026-07-28 13:26:53 +0300

    fix: phpstan error for additionalEmails

--- src/Service/QuoteProgressService.php
+++ src/Service/QuoteProgressService.php
@@ -12,18 +12,23 @@ class QuoteProgressService
     public function __construct(private readonly string $expireUnit, private readonly int $expireValue, private ?QuoteProgressAwareMailable $mailable = null) {}
 
     /**
-     * @param  array<array-key,string>  $data
+     * @param  array<string, mixed>  $data
      */
     public function create(array $data): QuoteProgress
     {
+        $additionalEmails = $data['additionalEmails'] ?? null;
+        if (! is_array($additionalEmails)) {
+            $additionalEmails = null;
+        }
+
         $data['expire_at'] = Carbon::now()->add($this->expireUnit, $this->expireValue);
-        $data['additional_emails'] = $data['additionalEmails'] ?? null;
+        $data['additional_emails'] = $additionalEmails;
 
         $quoteProgress = new QuoteProgress($data);
         $quoteProgress->save();
 
         if (isset($this->mailable)) {
-            $recipients = array_merge([$data['email']], $data['additionalEmails'] ?? []);
+            $recipients = array_merge([$data['email']], $additionalEmails ?? []);
             Mail::to($recipients)->queue($this->mailable->setQuoteProgress($quoteProgress));
         }
 
