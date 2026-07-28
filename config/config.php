a822fa6c03be63facce7aeb386c26a0ff9bf4688
mominsid <sid@jauntin.com>
2026-07-28 13:21:55 -0400

    Small updates to documentation (#17)

--- config/config.php
+++ config/config.php
@@ -7,6 +7,6 @@ return [
         'grace_period' => env('SAVING_QUOTE_EXPIRE_GRACE_PERIOD', 0),
     ],
     'max_additional_emails' => env('SAVING_QUOTE_MAX_ADDITIONAL_EMAILS', 4),
-    'mailable' => null, // Class name of the mailable to send after a quote is saved.
-    'validator' => null,
+    'mailable' => null, // Class name implementing QuoteProgressAwareMailable to send after a quote is saved.
+    'validator' => null, // Class name implementing QuoteProgressValidationRules.
 ];
